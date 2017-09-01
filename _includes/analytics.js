3079be503db5db1489b97d19f8fda57002fdbcca
“fujungao” <fujungao@gmail.com>
2017-09-01 21:45:54 +0200

    谷歌分析

---
+++ _includes/analytics.js
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl380\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <script>\
+  (function(i,s,o,g,r,a,m)\{i['GoogleAnalyticsObject']=r;i[r]=i[r]||function()\{\
+  (i[r].q=i[r].q||[]).push(arguments)\},i[r].l=1*new Date();a=s.createElement(o),\
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
+  \})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');\
+\
+  ga('create', 'UA-105687507-1', 'auto');\
+  ga('send', 'pageview');\
+\
+</script>}
